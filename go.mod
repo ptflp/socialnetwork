8338dd23d7722fbd8e2e5f3735689b58c0b57581
ptflp <globallinkliberty@gmail.com>
2021-07-01 04:18:38 +0900

    add preflight cors request middleware

--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.16
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/cors v1.2.0
 	github.com/go-redis/redis/v8 v8.10.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/json-iterator/go v1.1.11
 	github.com/nyaruka/phonenumbers v1.0.70
-	github.com/rs/cors v1.7.0
 	github.com/spf13/viper v1.8.0
 	github.com/subosito/gotenv v1.2.0
 	go.uber.org/zap v1.17.0
--- server/router.go
+++ server/router.go
@@ -3,6 +3,8 @@ package server
 import (
 	"net/http"
 
+	"github.com/go-chi/cors"
+
 	"gitlab.com/InfoBlogFriends/server/config"
 
 	"gitlab.com/InfoBlogFriends/server/handlers"
@@ -19,8 +21,18 @@ func NewRouter(services *Services, components *HandlerComponents, cfg *config.Co
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Recoverer)
 
-	openAllCors := middlewares.NewCors()
-	r.Use(openAllCors.OpenAllCors)
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	authHandler := handlers.NewAuthHandler(components.Responder, services.AuthService, components.Logger)
 
