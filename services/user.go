e710b2e8fca67c65b7559678307d29637b8b3618
ptflp <globallinkliberty@gmail.com>
2021-07-23 00:07:20 +0900

    email recover complete

--- services/user.go
+++ services/user.go
@@ -1,11 +1,16 @@
 package services
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"html/template"
 	"math/rand"
+	"net/url"
 	"time"
+
+	"gitlab.com/InfoBlogFriends/server/email"
 
 	"gitlab.com/InfoBlogFriends/server/utils"
 
@@ -23,9 +28,9 @@ import (
 )
 
 const (
-	PhoneRecoverKey   = "phone:recover:%s"
-	PhoneRecoveryUUID = "P"
-	RecoveryIDKey     = "recover:id:%s"
+	PhoneRecoverKey      = "phone:recover:%s"
+	PasswordRecoveryUUID = "R"
+	RecoveryIDKey        = "recover:id:%s"
 )
 
 type User struct {
@@ -149,6 +154,23 @@ func (u *User) SetPassword(ctx context.Context, setPasswordReq request.SetPasswo
 	return u.userRepository.SetPassword(ctx, user)
 }
 
+func (u *User) prepareRecoveryTemplate(recoverUrl string) (bytes.Buffer, error) {
+	tmpl, err := template.ParseFiles("./templates/password_recover.html")
+	if err != nil {
+		u.Logger().Error("recover password template parse", zap.Error(err))
+		return bytes.Buffer{}, err
+	}
+	type PasswordRecover struct {
+		PasswordRecover string
+	}
+
+	b := bytes.Buffer{}
+
+	err = tmpl.Execute(&b, PasswordRecover{PasswordRecover: recoverUrl})
+
+	return b, err
+}
+
 func (u *User) Subscribe(ctx context.Context, user infoblog.User, subscribeRequest request.UserIDRequest) error {
 	sub, err := u.userRepository.Find(ctx, infoblog.User{UUID: subscribeRequest.UUID})
 	if err != nil {
@@ -249,6 +271,28 @@ func (u *User) PasswordRecover(ctx context.Context, req request.PasswordRecoverR
 			return err
 		}
 		// send email
+		var recoverUrl string
+		recoverUrl, _, err = u.generateRecoverUrl(user)
+		if err != nil {
+			return err
+		}
+
+		var body bytes.Buffer
+		body, err = u.prepareRecoveryTemplate(recoverUrl)
+		if err != nil {
+			return err
+		}
+
+		msg := email.NewMessage()
+		msg.SetSubject("Восстановление пароля")
+		msg.SetType(email.TypeHtml)
+		msg.SetReceiver(user.Email.String)
+		msg.SetBody(body)
+
+		err = u.Email().Send(msg)
+		if err != nil {
+			return err
+		}
 	}
 
 	if user.Phone.Valid {
@@ -297,11 +341,10 @@ func (u *User) CheckPhoneCode(ctx context.Context, req request.CheckPhoneCodeReq
 		return request.RecoverChekPhoneResponse{}, err
 	}
 
-	recoverID, err := utils.ProjectUUIDGen(PhoneRecoveryUUID)
+	recoverID, err := u.GenerateRecoverID(user)
 	if err != nil {
 		return request.RecoverChekPhoneResponse{}, err
 	}
-	u.Cache().Set(fmt.Sprintf(RecoveryIDKey, recoverID), &user.UUID, 15*time.Minute)
 
 	return request.RecoverChekPhoneResponse{
 		Success: true,
@@ -309,6 +352,16 @@ func (u *User) CheckPhoneCode(ctx context.Context, req request.CheckPhoneCodeReq
 			RecoverID: recoverID,
 		},
 	}, nil
+}
+
+func (u *User) GenerateRecoverID(user infoblog.User) (string, error) {
+	recoverID, err := utils.ProjectUUIDGen(PasswordRecoveryUUID)
+	if err != nil {
+		return "", err
+	}
+	u.Cache().Set(fmt.Sprintf(RecoveryIDKey, recoverID), &user.UUID, 15*time.Minute)
+
+	return recoverID, err
 }
 
 func (u *User) PasswordReset(ctx context.Context, req request.PasswordResetRequest) error {
@@ -332,6 +385,22 @@ func (u *User) PasswordReset(ctx context.Context, req request.PasswordResetReque
 	}
 
 	return err
+}
+
+func (u *User) generateRecoverUrl(user infoblog.User) (string, string, error) {
+
+	recoverID, err := u.GenerateRecoverID(user)
+	if err != nil {
+		return "", "", err
+	}
+
+	uri, err := url.Parse(u.Config().App.FrontEnd)
+	if err != nil {
+		return "", "", err
+	}
+	uri.Path = fmt.Sprintf("profile/password/%s", recoverID)
+
+	return uri.String(), recoverID, err
 }
 
 func extractUser(ctx context.Context) (infoblog.User, error) {
