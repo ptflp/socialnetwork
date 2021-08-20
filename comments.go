69d15c5e3d807598e967342f53d4aedd23a4621c
gudicon <ns.petrov@s-vfu.ru>
2021-08-20 19:54:22 +0900

    add Comments struct

---
+++ comments.go
@@ -0,0 +1,31 @@
+package infoblog
+
+import (
+	"context"
+	"time"
+
+	"gitlab.com/InfoBlogFriends/server/types"
+)
+
+type Comments struct {
+	UUID        types.NullUUID   `json:"uuid" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null"`
+	ID          types.NullInt64  `json:"id" db:"id" ops:"update,create" orm_type:"int" orm_default:"not null"`
+	Body        types.NullString `json:"body" db:"body" ops:"update,create" orm_type:"varchar(511)"`
+	Type        types.NullInt64  `json:"type" db:"type" ops:"update,create" orm_type:"int" orm_default:"not null"`
+	ForeignUUID types.NullUUID   `json:"foreign_id" db:"foreign_uuid" ops:"update,create" orm_type:"binary(16)"`
+	UserUUID    types.NullUUID   `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null"`
+	UpdatedAt   time.Time        `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
+	CreatedAt   time.Time        `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
+}
+
+func (c Comments) TableName() string {
+	return "comments"
+}
+
+type CommentsRepository interface {
+	Update(ctx context.Context, comments Comments) error
+	Find(ctx context.Context, comments Comments) (Comments, error)
+	FindAll(ctx context.Context) ([]Comments, error)
+	FindLimitOffset(ctx context.Context, limit, offset uint64) ([]Comments, error)
+	CreateComments(ctx context.Context, comments Comments) error
+}
