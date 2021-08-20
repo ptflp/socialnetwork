f0d4341b07b547705f2f4758dde8e3cb172f25f6
gudicon <ns.petrov@s-vfu.ru>
2021-08-20 19:36:02 +0900

    add ChatParticipants struct

--- chat_messages.go
+++ chat_messages.go
@@ -2,7 +2,6 @@ package infoblog
 
 import (
 	"context"
-	"database/sql"
 	"time"
 
 	"gitlab.com/InfoBlogFriends/server/types"
@@ -10,11 +9,10 @@ import (
 
 type ChatMessages struct {
 	UUID      types.NullUUID   `json:"uuid" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null primary key"`
-	ChatUUID  types.NullUUID   `json:"chat_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null primary key"`
-	UserUUID  types.NullUUID   `json:"user_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null primary key"`
+	ChatUUID  types.NullUUID   `json:"chat_id" db:"uuid" ops:"create" orm_type:"binary(16)"`
+	UserUUID  types.NullUUID   `json:"user_id" db:"uuid" ops:"create" orm_type:"binary(16)"`
 	CreatedAt time.Time        `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
 	UpdatedAt time.Time        `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
-	DeletedAt sql.NullTime     `json:"deleted_at" db:"deleted_at" orm_type:"timestamp" orm_default:"null" orm_index:"index"`
 	Message   types.NullString `json:"message" db:"message" ops:"update,create" orm_type:"varchar(233)"  orm_default:"not null"`
 }
 
---
+++ chat_participants.go
@@ -0,0 +1,26 @@
+package infoblog
+
+import (
+	"context"
+	"time"
+
+	"gitlab.com/InfoBlogFriends/server/types"
+)
+
+type ChatParticipants struct {
+	ChatUUID types.NullUUID `json:"chat_id" db:"uuid" ops:"create" orm_type:"binary(16)"`
+	UserUUID types.NullUUID `json:"user_id" db:"uuid" ops:"create" orm_type:"binary(16)"`
+	JoinedAt time.Time      `json:"joined_at" db:"joined_at" orm_type:"timestamp" orm_default:"CURRENT_TIMESTAMP not null" orm_index:"index"`
+}
+
+func (u ChatParticipants) TableName() string {
+	return "chatParticipants"
+}
+
+type ChatParticipantsRepository interface {
+	Update(ctx context.Context, chatParticipants ChatParticipants) error
+	Find(ctx context.Context, chatParticipants ChatParticipants) (ChatParticipants, error)
+	FindAll(ctx context.Context) ([]ChatParticipants, error)
+	FindLimitOffset(ctx context.Context, limit, offset uint64) ([]ChatParticipants, error)
+	CreateChatParticipants(ctx context.Context, chatParticipants ChatParticipants) error
+}
