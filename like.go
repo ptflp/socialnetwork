eeddb23bd512afc5ce3e8292b8f658cb7dd546a9
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:10:13 +0900

    update Like struct

--- like.go
+++ like.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Like struct {
-	Type        int64          `json:"type" db:"type" ops:"create" orm_type:"int"`
-	ForeignUUID types.NullUUID `json:"foreign_id" db:"foreign_uuid" ops:"create" orm_type:"binary(16)"`
-	UserUUID    types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)"`
-	LikerUUID   types.NullUUID `json:"liker_id" db:"liker_uuid" ops:"create" orm_type:"binary(16)"`
-	Active      types.NullBool `json:"active" db:"active" ops:"create" orm_type:"boolean"`
+	Type        int64          `json:"type" db:"type" ops:"create" orm_type:"int" orm_default:"null"`
+	ForeignUUID types.NullUUID `json:"foreign_id" db:"foreign_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	UserUUID    types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	LikerUUID   types.NullUUID `json:"liker_id" db:"liker_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	Active      types.NullBool `json:"active" db:"active" ops:"create" orm_type:"boolean" orm_default:"null"`
 	CreatedAt   time.Time      `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
 	UpdatedAt   time.Time      `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
 }
