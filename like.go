f6158f25391789e385ce39afde77b38f5a1eae9b
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:07:19 +0900

    update Like struct

--- like.go
+++ like.go
@@ -9,12 +9,12 @@ import (
 
 type Like struct {
 	Type        int64          `json:"type" db:"type" ops:"create"`
-	ForeignUUID types.NullUUID `json:"foreign_id" db:"foreign_uuid" ops:"create"`
-	UserUUID    types.NullUUID `json:"user_id" db:"user_uuid" ops:"create"`
-	LikerUUID   types.NullUUID `json:"liker_id" db:"liker_uuid" ops:"create"`
-	Active      types.NullBool `json:"active" db:"active" ops:"create"`
-	CreatedAt   time.Time      `json:"created_at" db:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at" db:"updated_at"`
+	ForeignUUID types.NullUUID `json:"foreign_id" db:"foreign_uuid" ops:"create" orm_type:"binary(16)"`
+	UserUUID    types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)"`
+	LikerUUID   types.NullUUID `json:"liker_id" db:"liker_uuid" ops:"create" orm_type:"binary(16)"`
+	Active      types.NullBool `json:"active" db:"active" ops:"create" orm_type:"boolean"`
+	CreatedAt   time.Time      `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
+	UpdatedAt   time.Time      `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
 }
 
 func (l Like) TableName() string {
