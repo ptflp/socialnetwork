aeb421b4dc9f08f079bb1b1884a34035b0a90027
ptflp <globallinkliberty@gmail.com>
2021-08-23 18:50:52 +0900

    fix default db field

--- chat_participant.go
+++ chat_participant.go
@@ -11,7 +11,7 @@ type ChatParticipant struct {
 	ChatUUID types.NullUUID `json:"chat_id" db:"chat_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
 	UserUUID types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
 	Active   types.NullBool `json:"active" db:"active" ops:"create,update" orm_type:"boolean" orm_default:"null"`
-	JoinedAt time.Time      `json:"joined_at" db:"joined_at" orm_type:"timestamp" orm_default:"CURRENT_TIMESTAMP not null" orm_index:"index"`
+	JoinedAt time.Time      `json:"joined_at" db:"joined_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
 }
 
 func (c ChatParticipant) TableName() string {
