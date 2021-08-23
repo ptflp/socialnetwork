a493b24fd45588cb75aedf1919cc8840cb7fba18
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:40:04 +0900

    add chatParticipant crud

--- chat_participant.go
+++ chat_participant.go
@@ -7,20 +7,21 @@ import (
 	"gitlab.com/InfoBlogFriends/server/types"
 )
 
-type ChatParticipants struct {
+type ChatParticipant struct {
 	ChatUUID types.NullUUID `json:"chat_id" db:"chat_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
 	UserUUID types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	Active   types.NullBool `json:"active" db:"active" ops:"create,update" orm_type:"boolean" orm_default:"null"`
 	JoinedAt time.Time      `json:"joined_at" db:"joined_at" orm_type:"timestamp" orm_default:"CURRENT_TIMESTAMP not null" orm_index:"index"`
 }
 
-func (c ChatParticipants) TableName() string {
+func (c ChatParticipant) TableName() string {
 	return "chatParticipants"
 }
 
-type ChatParticipantsRepository interface {
-	Update(ctx context.Context, chatParticipants ChatParticipants) error
-	Find(ctx context.Context, chatParticipants ChatParticipants) (ChatParticipants, error)
-	FindAll(ctx context.Context) ([]ChatParticipants, error)
-	FindLimitOffset(ctx context.Context, limit, offset uint64) ([]ChatParticipants, error)
-	CreateChatParticipants(ctx context.Context, chatParticipants ChatParticipants) error
+type ChatParticipantRepository interface {
+	Create(ctx context.Context, chatParticipant ChatParticipant) error
+	Find(ctx context.Context, chatParticipant ChatParticipant) (ChatParticipant, error)
+	Update(ctx context.Context, chatParticipant ChatParticipant) error
+	Delete(ctx context.Context, chatParticipant ChatParticipant) error
+	List(ctx context.Context, limit, offset uint64) ([]ChatParticipant, error)
 }
--- repository.go
+++ repository.go
@@ -1,14 +1,15 @@
 package infoblog
 
 type Repositories struct {
-	Files        FileRepository
-	Posts        PostRepository
-	Users        UserRepository
-	Subscribers  SubscriberRepository
-	Likes        LikeRepository
-	Chats        ChatRepository
-	ChatMessages ChatMessagesRepository
-	Events       EventRepository
+	Files           FileRepository
+	Posts           PostRepository
+	Users           UserRepository
+	Subscribers     SubscriberRepository
+	Likes           LikeRepository
+	Chats           ChatRepository
+	ChatMessages    ChatMessagesRepository
+	Events          EventRepository
+	ChatParticipant ChatParticipantsRepository
 }
 
 type Entity interface {
