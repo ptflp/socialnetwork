e6e409a2976823a3b115e9ad1a15a930cdca972d
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:12:43 +0900

    update

--- chat_message.go
+++ chat_message.go
@@ -9,12 +9,12 @@ import (
 
 type ChatMessages struct {
 	UUID      types.NullUUID `json:"message_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null primary key"`
-	ChatUUID  types.NullUUID `json:"chat_id" db:"chat_uuid" ops:"create" orm_type:"binary(16)"`
-	UserUUID  types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)"`
-	Active    types.NullBool `json:"active" db:"active" ops:"create,update" orm_type:"boolean"`
+	ChatUUID  types.NullUUID `json:"chat_id" db:"chat_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	UserUUID  types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	Active    types.NullBool `json:"active" db:"active" ops:"create,update" orm_type:"boolean" orm_default:"not null"`
 	CreatedAt time.Time      `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
 	UpdatedAt time.Time      `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
-	Message   string         `json:"message" db:"message" ops:"update,create" orm_type:"varchar(233)"`
+	Message   string         `json:"message" db:"message" ops:"update,create" orm_type:"varchar(233)" orm_default:"not null"`
 }
 
 func (c ChatMessages) TableName() string {
