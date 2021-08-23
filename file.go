add0629a5464cefca6e4776c28677cd2d7b1ccb2
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:19:25 +0900

    update

--- file.go
+++ file.go
@@ -8,15 +8,15 @@ import (
 )
 
 type File struct {
-	UUID        types.NullUUID `json:"file_id" db:"uuid" ops:"create"`
-	Type        int64          `json:"type" db:"type" ops:"create,update"`
-	ForeignUUID types.NullUUID `json:"foreign_uuid" db:"foreign_uuid" ops:"create,update"`
-	UserUUID    types.NullUUID `json:"user_id" db:"user_uuid" ops:"create"`
-	Dir         string         `json:"dir" db:"dir" ops:"create,update"`
-	Name        string         `json:"name" db:"name" ops:"create"`
-	Active      int64          `json:"active" db:"active" ops:"create,update"`
-	CreatedAt   time.Time      `json:"created_at" db:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at" db:"updated_at"`
+	UUID        types.NullUUID `json:"file_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null"`
+	Type        int64          `json:"type" db:"type" ops:"create,update" orm_type:"int" orm_default:"not null"`
+	ForeignUUID types.NullUUID `json:"foreign_uuid" db:"foreign_uuid" ops:"create,update" orm_type:"binary(16)" orm_default:"null"`
+	UserUUID    types.NullUUID `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"null"`
+	Dir         string         `json:"dir" db:"dir" ops:"create,update" orm_type:"varchar(100)" orm_default:"not null"`
+	Name        string         `json:"name" db:"name" ops:"create" orm_type:"varchar(50)" orm_default:"not null"`
+	Active      int64          `json:"active" db:"active" ops:"create,update" orm_type:"boolean" orm_default:"null"`
+	CreatedAt   time.Time      `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
+	UpdatedAt   time.Time      `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
 }
 
 func (f File) TableName() string {
