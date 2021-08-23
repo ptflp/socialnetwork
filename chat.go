f97d21cb57fd1844b32a25c5023e1008cb0e2288
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:12:43 +0900

    update

--- chat.go
+++ chat.go
@@ -9,7 +9,7 @@ import (
 
 type Chat struct {
 	UUID      types.NullUUID  `json:"chat_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null primary key"`
-	Type      types.NullInt64 `json:"type" db:"type" ops:"update,create" orm_type:"int"`
+	Type      types.NullInt64 `json:"type" db:"type" ops:"update,create" orm_type:"int" `
 	CreatedAt time.Time       `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
 }
 
