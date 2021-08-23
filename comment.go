7095016e36c9e2f02f55a1715586c1c3c93cf7d3
gudicon <ns.petrov@s-vfu.ru>
2021-08-23 17:14:01 +0900

    update

--- comment.go
+++ comment.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Comments struct {
-	UUID        types.NullUUID   `json:"comment_id" db:"uuid" ops:"create" orm_type:"binary(16)"`
-	ID          int64            `json:"id" db:"id" ops:"update,create" orm_type:"int"`
-	Body        types.NullString `json:"body" db:"body" ops:"update,create" orm_type:"varchar(511)"`
-	Type        int64            `json:"type" db:"type" ops:"update,create" orm_type:"int"`
-	ForeignUUID types.NullUUID   `json:"foreign_id" db:"foreign_uuid" ops:"update,create" orm_type:"binary(16)"`
+	UUID        types.NullUUID   `json:"comment_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null"`
+	ID          int64            `json:"id" db:"id" ops:"update,create" orm_type:"int" orm_default:"not null"`
+	Body        types.NullString `json:"body" db:"body" ops:"update,create" orm_type:"varchar(511)" orm_default:"null"`
+	Type        int64            `json:"type" db:"type" ops:"update,create" orm_type:"int" orm_default:"not null"`
+	ForeignUUID types.NullUUID   `json:"foreign_id" db:"foreign_uuid" ops:"update,create" orm_type:"binary(16)" orm_default:"null"`
 	UserUUID    types.NullUUID   `json:"user_id" db:"user_uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null"`
 	UpdatedAt   time.Time        `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
 	CreatedAt   time.Time        `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
