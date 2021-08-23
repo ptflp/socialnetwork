a526a08008c1400947a6d3b65214105738906124
ptflp <globallinkliberty@gmail.com>
2021-08-23 18:07:43 +0900

    fix services

--- services/services.go
+++ services/services.go
@@ -12,7 +12,6 @@ type Services struct {
 	User *User
 	Post *Post
 	File *File
-	Chat *Chat
 }
 
 func NewServices(cmps components.Componenter, repositories infoblog.Repositories) *Services {
@@ -20,13 +19,11 @@ func NewServices(cmps components.Componenter, repositories infoblog.Repositories
 	file := NewFileService(repositories.Files)
 	post := NewPostService(repositories, file, cmps.Decoder(), &services)
 	user := NewUserService(repositories, post, cmps, file)
-	chat := NewChatService(repositories)
 
 	services.AuthService = auth.NewAuthService(repositories, cmps)
 	services.User = user
 	services.Post = post
 	services.File = file
-	services.Chat = chat
 
 	return &services
 }
