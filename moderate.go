88a3e38478bcab85e8c01c30dff44f2caff5eb01
ptflp <globallinkliberty@gmail.com>
2021-08-27 18:17:29 +0900

    add moderate

---
+++ moderate.go
@@ -0,0 +1,23 @@
+package infoblog
+
+import (
+	"time"
+
+	"gitlab.com/InfoBlogFriends/server/types"
+)
+
+type Moderate struct {
+	UUID      types.NullUUID  `json:"like_id" db:"uuid" ops:"create" orm_type:"binary(16)" orm_default:"not null primary key"`
+	Type      types.NullInt64 `json:"type" db:"type" ops:"create" orm_type:"int" orm_default:"null"`
+	Active    types.NullBool  `json:"active" db:"active" ops:"create" orm_type:"boolean" orm_default:"null"`
+	CreatedAt time.Time       `json:"created_at" db:"created_at" orm_type:"timestamp" orm_default:"default (now()) not null" orm_index:"index"`
+	UpdatedAt time.Time       `json:"updated_at" db:"updated_at" orm_type:"timestamp" orm_default:"default (now()) null on update CURRENT_TIMESTAMP" orm_index:"index"`
+}
+
+func (l Moderate) OnCreate() string {
+	return ""
+}
+
+func (l Moderate) TableName() string {
+	return "moderates"
+}
